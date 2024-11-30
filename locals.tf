4a11e21231ec4beeeadf3e0a7ac7bb4d46596bfc
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2024-11-30 18:04:26 +0100

    up

--- locals.tf
+++ locals.tf
@@ -56,7 +56,7 @@ locals {
     runcmd = [
       "mkdir ${local.cache_mount_path}",
       "mount -o discard,defaults /dev/disk/by-id/scsi-0HC_Volume_${var.volume_cache_id} ${local.cache_mount_path}",
-      "sed -i 's|\"data-root\": *\"/[^\"]*\"|\"data-root\": \"${local.cache_mount_path}\"|g' /etc/docker/daemon.json",
+      "sed -i 's|/var/lib|${local.cache_mount_path}|g' /etc/docker/daemon.json",
       "systemctl restart docker",
       "docker compose -f /runner/compose.yaml up -d",
     ]
