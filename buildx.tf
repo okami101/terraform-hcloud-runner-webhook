d3a7a547bc90ab795dd44cf0cc1a0175f6309587
Adrien Beaudouin <3679080+adr1enbe4udou1n@users.noreply.github.com>
2025-06-07 21:18:07 +0200

    remove vol cache

--- buildx.tf
+++ buildx.tf
@@ -1,5 +1,5 @@
 resource "hcloud_server" "buildx" {
-  for_each    = { for server in local.buildx_servers : server.server_name => server }
+  for_each    = { for server in var.buildx_servers : server.server_name => server }
   name        = each.value.server_name
   server_type = each.value.server_type
   location    = each.value.server_location
@@ -17,22 +17,4 @@ resource "hcloud_server" "buildx" {
       image
     ]
   }
-  user_data = <<-EOT
-#cloud-config
-${yamlencode({
-  write_files = local.cloud_init_write_files
-  runcmd = [
-    "mkdir ${each.value.cache_mount_path}",
-    "mount -o discard,defaults /dev/disk/by-id/scsi-0HC_Volume_${each.value.volume_cache_id} ${each.value.cache_mount_path}",
-    "sed -i 's|/var/lib|${each.value.cache_mount_path}|g' ${local.docker_config_file_path}",
-    "systemctl restart docker",
-  ]
-})}
-EOT
-}
-
-resource "hcloud_volume_attachment" "buildx_cache" {
-  for_each  = { for server in local.buildx_servers : server.server_name => server }
-  server_id = hcloud_server.buildx[each.key].id
-  volume_id = each.value.volume_cache_id
 }
